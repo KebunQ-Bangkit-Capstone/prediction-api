1bee0629a4e0834d8acaa140f649654315c6ce0c
riveleus <riveleus@gmail.com>
2024-12-11 11:40:47 +0700

    debug

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ from google.cloud import storage
 
 def preprocess_image(image):
     image = image.resize(224, 224)
-    return np.array(image)
+    image_array = np.array(image)
+    return np.expand_dims(image_array, axis=0)
 
 def download_model_from_gcs(source_blob_name, destination_file_name):
     storage_client = storage.Client()
